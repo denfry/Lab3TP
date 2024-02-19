a835eba9b1db8584756edf5dc9fdc4a7bf9439e8
denfry <daniilyurkov16@gmail.com>
2024-02-20 00:13:51 +0300

    Добавьте файлы проекта.

---
+++ Lab2TP/TriangleManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Lab2TP.MainForm;
+
+namespace Lab2TP
+{
+    internal class TriangleManager
+    {
+        private static isoscelesTriangle activeTriangle;
+
+        public static isoscelesTriangle GetActiveTriangle()
+        {
+            return activeTriangle;
+        }
+
+        public static void SetActiveTriangle(isoscelesTriangle triangle)
+        {
+            activeTriangle = triangle;
+        }
+    }
+}
